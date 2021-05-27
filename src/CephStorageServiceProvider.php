2d1a96aa8250d89c1ce3dd2455eb0f73dce180f0
Imran Hassan <imrantune@users.noreply.github.com>
2021-05-27 15:41:28 +0500

    Update CephStorageServiceProvider.php
    Add more option for ACL / Visibility

--- src/CephStorageServiceProvider.php
+++ src/CephStorageServiceProvider.php
@@ -13,6 +13,10 @@ use League\Flysystem\Filesystem;
 class CephStorageServiceProvider extends ServiceProvider
 {
     const CEPH_BASE_URL = 'https://s3.aws.com';
+    const CEPH_VISIBILITY = 'private';
+    const CEPH_PREFIX = 'panther';
+    const CEPH_DEBUG = false;
+    const CEPH_ACL = 'private';
 
     /**
      * Bootstrap the application services.
@@ -34,12 +38,26 @@ class CephStorageServiceProvider extends ServiceProvider
     {
         //
         Storage::extend('ceph', function ($app, $config) {
+
             $config['endpoint'] = isset($config['base_url']) ? $config['base_url'] : self::CEPH_BASE_URL;
+
             $config['use_path_style_endpoint'] = true;
+            
+
+            $config['debug'] = isset($config['debug']) ? $config['debug'] : self::CEPH_DEBUG;;
+            
+            $prefix =  isset($config['prefix']) ? $config['prefix'] : self::CEPH_PREFIX;
+
+            $options = [];
+
+            $options['visibility'] =  isset($config['visibility']) ? $config['visibility'] : self::CEPH_VISIBILITY;
+
             $client = new S3Client($config);
 
-            return new Filesystem(new AwsS3Adapter($client, $config['bucket']));
+
+            $options['ACL'] =  isset($config['ACL']) ? $config['ACL'] : self::CEPH_ACL;
+
+            return new Filesystem(new AwsS3Adapter($client, $config['bucket'] , $prefix, $options ) );
         });
     }
 }
-
